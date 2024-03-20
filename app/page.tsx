000c1592919e931f3b244fbb668c0bbd28884f94
JamesSimel <jameskarinosimel@gmail.com>
2024-03-20 21:42:48 +0300

    removed unneccesary committed files

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
